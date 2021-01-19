492f3c911b32bae3cd25a3f637086afe0dcd9235
cc <478959472@qq.com>
2021-01-19 11:40:48 +0800

    修改包名

---
+++ src/main/java/com/montnets/creation/platform/modules/ums/service/UmsRoleMenuRelationService.java
@@ -0,0 +1,11 @@
+package com.montnets.creation.platform.modules.ums.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.montnets.creation.platform.modules.ums.model.UmsRoleMenuRelation;
+
+/**
+ * 角色菜单关系管理Service
+ * Created by macro on 2020/8/21.
+ */
+public interface UmsRoleMenuRelationService extends IService<UmsRoleMenuRelation> {
+}
